3714f84b1b255e38c0eff60cf65936c3ef06febc
Aniketh Ganta <aniganta7@gmail.com>
2025-11-03 04:00:48 -0600

    Basic Modular ML

---
+++ ml/Entry.py
@@ -0,0 +1,10 @@
+import DataFunctions, TrainingFunctions, TestingFunctions
+import pandas as pd
+
+path = "./data/all_stocks_5yr.csv"
+features = ["open", "high", "low", "close", "volume"]
+
+df = DataFunctions.getDataframe(path)
+df = df[features]
+
+history = TrainingFunctions.trainTorchLSTM(df, name="testmodel", epochs=1, pred_col=3)
